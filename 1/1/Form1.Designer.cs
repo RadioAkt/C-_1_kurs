9930b0f61f821898efdc192a038abca698ad68f1
RadioAkt <109156862+RadioAkt@users.noreply.github.com>
2026-03-17 19:22:52 +0300

    Add files via upload

---
+++ 1/1/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "m = ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(53, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "n =";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(109, 37);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 2;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(109, 70);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 3;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(53, 138);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 23);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Задание 1.1";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(152, 138);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 23);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Задание 1.2";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(356, 138);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 23);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "Задание 2";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(452, 138);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 23);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "Задание 3";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(356, 168);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(75, 17);
+            this.checkBox5.TabIndex = 8;
+            this.checkBox5.Text = "Пример 1";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(356, 192);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(75, 17);
+            this.checkBox6.TabIndex = 9;
+            this.checkBox6.Text = "Пример 2";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(452, 168);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(75, 17);
+            this.checkBox7.TabIndex = 10;
+            this.checkBox7.Text = "Пример 1";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(452, 192);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(75, 17);
+            this.checkBox8.TabIndex = 11;
+            this.checkBox8.Text = "Пример 2";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(56, 241);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "x =";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(249, 52);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "очиститть";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(56, 267);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "y =";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(51, 294);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "z =";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(98, 233);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 16;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(98, 267);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 17;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(98, 294);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 18;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(265, 267);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 19;
+            this.button11.Text = "очистить";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(788, 435);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ 1/1/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox6.Text == "" & textBox7.Text == "")
+            {
+                MessageBox.Show("Введите значения m и n!!!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            } else if (textBox6.Text == "")
+            {
+                MessageBox.Show("Введите значения m!!!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else if (textBox7.Text == "")
+            {
+                MessageBox.Show("Введите значение n!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            } else
+            {
+                double m = Double.Parse(textBox6.Text);
+                double n = Double.Parse(textBox7.Text);
+                double x = (Math.Sqrt(m) + 1.27) / (Math.Log(n + m));
+                double y = 0.8 * Math.Exp(0.1 * m);
+                double c = y + 3.2 * Math.Sin(x * y + 1.8) + Math.Sqrt(Math.Abs(x - Math.Log(2.6 + x)));
+                label8.Visible = true;
+                label9.Visible = true;
+                label10.Visible = true;
+                textBox8.Visible = true;
+                textBox9.Visible = true;
+                textBox10.Visible = true;
+                textBox8.Text = x.ToString();
+                textBox9.Text = y.ToString();
+                textBox10.Text = c.ToString();
+                label8.Text = "x = ";
+                label9.Text = "y = ";
+                label10.Text = "c = ";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "";
+            textBox7.Text = "";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox6.Text == "" & textBox7.Text == "")
+            {
+                MessageBox.Show("Введите значения m и n!!!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else if (textBox6.Text == "")
+            {
+                MessageBox.Show("Введите значения m!!!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else if (textBox7.Text == "")
+            {
+                MessageBox.Show("Введите значение n!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else
+            {
+                double m = Double.Parse(textBox6.Text);
+                double n = Double.Parse(textBox7.Text);
+                double x = Math.Exp(-Math.Sqrt(n / m));
+                double t = Math.Log(m / Math.Sqrt(n)) + 7.21;
+                double y = (Math.Cos(Math.Pow(x, 2) - Math.Sqrt(t) + Math.Sin(x - Math.Pow(t, 1 / 3)))) / (Math.Pow(Math.Sin(t - Math.Pow(x, 1 / 4)), 2) + 1.731);
+                label8.Visible = true;
+                label9.Visible = true;
+                label10.Visible = true;
+                textBox8.Visible = true;
+                textBox9.Visible = true;
+                textBox10.Visible = true;
+                textBox8.Text = x.ToString();
+                textBox9.Text = t.ToString();
+                textBox10.Text = y.ToString();
+                label8.Text = "x = ";
+                label9.Text = "t = ";
+                label10.Text = "y = ";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox6.Text == "" & textBox7.Text == "")
+            {
+                MessageBox.Show("Введите значения m и n!!!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else if (textBox6.Text == "")
+            {
+                MessageBox.Show("Введите значения m!!!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else if (textBox7.Text == "")
+            {
+                MessageBox.Show("Введите значение n!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            } else
+            {
+                if (checkBox5.Checked == false & checkBox6.Checked == false)
+                {
+                    MessageBox.Show("Выберите номер примера!!!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                }
+                else if (checkBox5.Checked == true & checkBox6.Checked == true)
+                {
+                    MessageBox.Show("Выберите только один из номеров примера!!!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                }
+                else
+                {
+                    if (checkBox5.Checked == true)
+                    {
+                        double m = Double.Parse(textBox6.Text);
+                        double n = Double.Parse(textBox7.Text);
+                        double x = Math.Exp(m / n);
+                        double y = Math.Cos(m / Math.Sqrt(n));
+                        double u = 0;
+                        if (x + y < x * y)
+                            u = x + y;
+                        else if (x + y > x * y)
+                            u = x * y;
+                        else
+                            u = 1;
+                        label8.Visible = true;
+                        label9.Visible = true;
+                        label10.Visible = true;
+                        textBox8.Visible = true;
+                        textBox9.Visible = true;
+                        textBox10.Visible = true;
+                        textBox8.Text = x.ToString();
+                        textBox9.Text = y.ToString();
+                        textBox10.Text = u.ToString();
+                        label8.Text = "x = ";
+                        label9.Text = "y = ";
+                        label10.Text = "u = ";
+                        checkBox5.Checked = false;
+                    }
+                    else if (checkBox6.Checked == true)
+                    {
+                        double m = Double.Parse(textBox6.Text);
+                        double n = Double.Parse(textBox7.Text);
+                        double x = 3 * Math.Cos(n / m);
+                        double z = 0;
+                        if (x < 1)
+                            z = x - 1.5;
+                        else if (1 <= x & x < 2)
+                            z = x + 2.5;
+                        else
+                            z = Math.Pow(x, 3) - 3.5;
+                        label8.Visible = true;
+                        label9.Visible = true;
+                        label10.Visible = false;
+                        textBox8.Visible = true;
+                        textBox9.Visible = true;
+                        textBox10.Visible = false;
+                        textBox8.Text = x.ToString();
+                        textBox9.Text = z.ToString();
+                        label8.Text = "x = ";
+                        label9.Text = "z = ";
+                        checkBox6.Checked = false;
+                    }
+                }
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox8.Text = "";
+            textBox9.Text = "";
+            textBox10.Text = "";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox6.Text == "" & textBox7.Text == "")
+            {
+                MessageBox.Show("Введите значения m и n!!!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else if (textBox6.Text == "")
+            {
+                MessageBox.Show("Введите значения m!!!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else if (textBox7.Text == "")
+            {
+                MessageBox.Show("Введите значение n!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else
+            {
+                if (checkBox7.Checked == false & checkBox8.Checked == false)
+                {
+                    MessageBox.Show("Выберите номер примера!!!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                }
+                else if (checkBox5.Checked == true & checkBox6.Checked == true)
+                {
+                    MessageBox.Show("Выберите только один из номеров примера!!!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                }
+                else
+                {
+                    if (checkBox7.Checked == true)
+                    {
+                        double m = Double.Parse(textBox6.Text);
+                        double n = Double.Parse(textBox7.Text);
+                        double y = 0;
+                        double z = 0;
+                        for (int k = 1; k <= 15; k++)
+                            y += Math.Pow(-1, k) / (2 * k + m);
+                        z = y * (y - 4) + 0.02 * n;
+                        label8.Visible = true;
+                        label9.Visible = true;
+                        label10.Visible = false;
+                        textBox8.Visible = true;
+                        textBox9.Visible = true;
+                        textBox10.Visible = false;
+                        textBox8.Text = y.ToString();
+                        textBox9.Text = z.ToString();
+                        label8.Text = "y = ";
+                        label9.Text = "z = ";
+                        checkBox7.Checked = false;
+                    } else if (checkBox8.Checked == true)
+                    {
+                        double m = Double.Parse(textBox6.Text);
+                        double n = Double.Parse(textBox7.Text);
+                        double v = 0;
+                        double u = 0;
+                        for (int k = 1; k <= 30; k++)
+                            v += Math.Pow(-1, k + 1) / (m * k + 1);
+                        v = m * v;
+                        u = Math.Pow(v, 2) + (2 * v) - (0.2 * n);
+                        label8.Visible = true;
+                        label9.Visible = true;
+                        label10.Visible = false;
+                        textBox8.Visible = true;
+                        textBox9.Visible = true;
+                        textBox10.Visible = false;
+                        textBox8.Text = v.ToString();
+                        textBox9.Text = u.ToString();
+                        label8.Text = "v = ";
+                        label9.Text = "u = ";
+                        checkBox8.Checked = false;
+                    }
+                }
+            }
+        }
+    }
+}
